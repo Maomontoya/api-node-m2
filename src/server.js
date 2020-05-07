0588eb06712bc8474d2e4beb60559982102875cd
Mauricio <mao.mv94@gmail.com>
2020-05-06 23:13:31 -0500

    server.js header

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import express, {json} from 'express'
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 //routes
 import IndexRoutes from './routes/index.routes'
@@ -16,5 +25,7 @@ app.use(json());
 //routes
 app.use(IndexRoutes);
 app.use('/appointment', AppointmentRoutes);
+
+
 
 export default app;
